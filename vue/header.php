e520117d95536d6034ec32fe5c1daad883147fc5
laurent <laurent.moriceau@gmail.com>
2020-05-18 21:26:24 +0200

    creation

---
+++ vue/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<html lang="FR-fr">
+
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="js/jquery-ui-1.12.1/jquery-ui.css">
+  <link rel="stylesheet" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+
+  <link rel="stylesheet" href="css/style_tablette.css">
+  <link rel="stylesheet" href="css/style_smartphone.css">
+
+  <title>Pour les Yeux d'Adam</title>
+
+</head>
+
+<body>
+
+<div class="uphead">
+    <a href="admin.php"><img src="data/logo.jpeg" class="Logo" alt="Logo du site" title="Logo du site" style="margin-right:5%;"></a>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
+
+  <a class="navbar-brand" href="index.php"><h1>Acceuil</h1></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="adam.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <h1>Adam's Story</h1>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="adam.php">Introduction</a>
+            <a class="dropdown-item" href="story.php">Story</a>
+            <div class="dropdown-divider"></div>
+
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="camsp.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <h1>CAMSP</h1>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="camsp.php">Présentation du Camsp</a>
+            <a class="dropdown-item" href="archiveEvenement.php">Evenement du Camsp</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Problématique du Camsp</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="filmsadListeAjout.php">
+            <h1>Films AD</h1>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="portefolio.php">
+            <h1>Portefolio</h1>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">
+            <h1>Contact</h1>
+          </a>
+        </li>
+
+
+      </ul>
+
+  </nav>
+<!-- barre search prete à activation/*
+     <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+-->
+    </div>
+  </div>

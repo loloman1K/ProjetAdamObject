3159025e3e145418bf9d8067193ecb98e3ca53bc
c.jackson <jacksoncurt11@gmail.com>
2020-05-29 11:08:09 +0200

    nav

--- office/vueadmin/header.php
+++ office/vueadmin/header.php
@@ -21,7 +21,7 @@
 <div class="uphead">
 <a href="admin.php"><img src="../data/logo.jpeg" class="Logo" alt="Logo du site" title="Logo du site" style="margin-right:5vw;"></a>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
   <a class="navbar-brand" href="../index.php"><h1>Acceuil</h1></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

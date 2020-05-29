b71d87d3efd98fa2ceaaa074275f697b1d3e1e8a
c.jackson <jacksoncurt11@gmail.com>
2020-05-29 10:37:33 +0200

    crud et vueadmin dans office

---
+++ office/vueadmin/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <script src="js/jquery-3.4.1.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</footer>
+
+</body>
+</html>

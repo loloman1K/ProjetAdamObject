65d450cef0573ec919577755a2b75cc101edc7be
c.jackson <jacksoncurt11@gmail.com>
2020-06-01 11:35:39 +0200

    Mise en page index.php

--- index.php
+++ index.php
@@ -7,23 +7,29 @@ require 'vue/header.php';
 
 
 
-  <div class="banni">
-      <h1>Pour les yeux d'Adam</h1>
-    </div>
+<div class="banni">
+  <h1>Pour les yeux d'Adam</h1>
+</div>
 
-      <div class="block1">
-        <img class="pictintroduction" src="data/naissanceAdam.jpg" title="Moi avec mon fils le jour de sa naissance " alt="le Papa d'Adam avec Adam le jour de sa naissance">
-            <div class="block2">
-                <h2>Introduction :</h2>
-                      <p classe="p">
-                            Ce site est dédié à mon fils Adam, Aveugle de naissance et malgré les opérations chirurgicale sa cécité n’a pas évolué elle reste Totale.<br/>
-    Avec son parcourt atypique, je décide de partagé nos information et l’évolution de notre fils, afin d’aider les familles vivant la même situation et échanger le cas échéances sur les meilleur pratiques facilitant la vie de personnes atteint par cette pathologie.
-<br/>
-    Pour information nous avons des jumeaux, une fille n’ayant aucun ennuie de santé et notre fils atteint de cécité à la naissance. Ils sont nés en février 2018.
-                        </p>
-                      </div>
-                    </div>
+<section class="block-container">
+  <article class="block1">
+    <img class="Laurent-Adam" src="data/naissanceAdam.jpg" title="Moi avec mon fils le jour de sa naissance " alt="le Papa d'Adam avec Adam le jour de sa naissance">
+  </article>
 
+  <article class="block2">
+    <h2 class="intro">Introduction</h2>
+    <p class="p-intro">
+      Ce site est dédié à mon fils Adam, Aveugle de naissance et malgré les opérations chirurgicale sa cécité
+      n’a pas évolué elle reste Totale.<br />
+      Avec son parcourt atypique, je décide de partagé nos information et l’évolution de notre fils,
+      afin d’aider les familles vivant la même situation et échanger le cas échéances sur les meilleur
+      pratiques facilitant la vie de personnes atteint par cette pathologie.
+      <br />
+      Pour information nous avons des jumeaux, une fille n’ayant aucun ennuie de santé et notre fils
+      atteint de cécité à la naissance. Ils sont nés en février 2018.
+    </p>
+  </article>
+</section>
 
 <a href="office/LoginForm.php">Login Admin</a>
 
@@ -31,4 +37,4 @@ require 'vue/header.php';
 
 require 'vue/footer.php';
 
- ?>
+?>

8423161c334b29383d86040fc834f93726248636
c.jackson <jacksoncurt11@gmail.com>
2020-06-02 09:24:14 +0200

    Nav vue Office

--- office/vueadmin/header.php
+++ office/vueadmin/header.php
@@ -18,7 +18,7 @@
 
 <body>
   
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
 
 <a class="navbar-brand" href="../index.php"><img src="../data/logo.jpeg" class="Logo" alt="Logo du site" title="Logo du site"></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

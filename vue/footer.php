2d5267fbf88020d89484b547a5d0d01a583e89de
laurent <laurent.moriceau@gmail.com>
2020-06-04 22:41:35 +0200

    footer client

--- vue/footer.php
+++ vue/footer.php
@@ -5,63 +5,61 @@
 
 <footer>
 
-
-  <nav class="navbar navbar-expand-lg navbar-light">
-
-  <a class="navbar-brand" href="../index.php"><img src="data/logo.jpeg" class="Logo" alt="Logo du site" title="Logo du site"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li>
-      <a class="navbar-brand" style= "margin-right: 3vw;" href="../index.php">Acceuil</a>
-    </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" style="margin-right: 3vw;" href="adam.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Adam's Story
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="adam.php">Introduction</a>
-            <a class="dropdown-item" href="story.php">Story</a>
-            <div class="dropdown-divider"></div>
-
+  <!-- Footer -->
+  <footer class="footer text-center" style="background-color:#212529; color:#bbd98f;">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4 mb-5 mb-lg-0">
+                  <h4 class="text-uppercase mb-4">Localisation</h4>
+                  <p class="lead mb-0">En région Lyonnaise
+                      </p>
+              </div>
+              <div class="col-md-4 mb-5 mb-lg-0">
+                  <h4 class="text-uppercase mb-4">Ma connectivité</h4>
+                  <ul class="row">
+                      <li style="color:#212529;">
+                          <a href="https://www.facebook.com/Vivre-avec-son-handicap-298044807262245">
+                        <img src="data/icons/facebook.png" style="width: 2vw; margin-left:2vw; margin-right: 2vw; margin-top: 2vh;">
+                      </a>
+                      </li>
+                      <li style="color:#212529;">
+                          <a href="https://www.youtube.com/channel/UCds9wEhLI2WsniWk4HMyDoA">
+                        <img src="data/icons/youtube.png" style="width: 2vw; margin-left:2vw; margin-right: 2vw; margin-top: 2vh;">
+                      </a>
+                      </li>
+                      <li style="color:#212529;">
+                          <a href="https://linkedin.com/in/laurent-moriceau-31258895">
+                        <img src="data/icons/linkedin.jpg" style="width: 2vw; margin-left:2vw; margin-right: 2vw; margin-top: 2vh;">
+                      </a>
+                      </li>
+                      <li style="color:#212529;">
+                          <a href="http://vivre-avec-son-handicap.fr/">
+                        <img src="data/icons/logovash.jpg" style="width: 2vw; margin-left:2vw; margin-right: 2vw; margin-top: 2vh;">
+                      </a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="col-md-4">
+                  <h4 class="text-uppercase mb-4">A Propos</h4>
+                  <p class="lead mb-0">Ce site à été créer par deux amis et collégue de la formation Handigital.<br/>Il s'agit de Curtis et Jean-Noêl et moi même (Laurent Papa de Adam)
+                      (<a href="https://handigital-formation.com/">Handigital</a>)</p>
+              </div>
           </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="camsp.php" id="navbarDropdown" style="margin-right: 3vw;" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            CAMSP
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="camsp.php">Présentation du Camsp</a>
-            <a class="dropdown-item" href="archiveEvenement.php">Evenement du Camsp</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Problématique du Camsp</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="filmsadListeAjout.php" style="margin-right: 3vw;">
-          Films AD
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="portefolio.php" style="margin-right: 3vw;">
-          Portefolio
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php" style="margin-right: 3vw;">
-          Contact
-          </a>
-        </li>
-      </ul>
-
-  </nav>
-
       </div>
-    </div>
+  </footer>
 
+  <div class="copyright py-4 text-center text-white">
+      <div class="container">
+          <small>Copyright &copy;<?php echo bloginfo('name') ?> <?php echo date('Y') ?></small>
+      </div>
+  </div>
+
+  <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+  <div class="scroll-to-top d-lg-none position-fixed ">
+      <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+          <i class="fa fa-chevron-up"></i>
+      </a>
+  </div>
 
 
 

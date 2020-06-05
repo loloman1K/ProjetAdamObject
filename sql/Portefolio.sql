ce94513f9acd30a14a15b53d2263f22e389c7664
c.jackson <jacksoncurt11@gmail.com>
2020-06-05 12:12:20 +0200

    sql

--- sql/Portefolio.sql
+++ sql/Portefolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 03 juin 2020 à 13:00
+-- Généré le :  ven. 05 juin 2020 à 10:10
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.4.0
 
@@ -30,25 +30,22 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `portefolio`;
 CREATE TABLE IF NOT EXISTS `portefolio` (
-  `portefolio_id` int(7) NOT NULL AUTO_INCREMENT,
+  `id_portefolio` int(7) NOT NULL AUTO_INCREMENT,
   `picture_portefolio` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `originPicture_portefolio` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `evenement_portefolio` text CHARACTER SET utf8 COLLATE utf8_general_ci,
   `lieu_portefolio` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `date_portefolio` date DEFAULT NULL,
   `other_portefolio` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  PRIMARY KEY (`portefolio_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_portefolio`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `portefolio`
 --
 
-INSERT INTO `portefolio` (`portefolio_id`, `picture_portefolio`, `originPicture_portefolio`, `evenement_portefolio`, `lieu_portefolio`, `date_portefolio`, `other_portefolio`) VALUES
-(1, '', '', NULL, '', '0000-00-00', ''),
-(2, '', '', NULL, NULL, '1991-07-18', NULL),
-(3, '', '', NULL, NULL, '1991-07-18', NULL),
-(4, '', '', NULL, NULL, '1991-07-18', NULL);
+INSERT INTO `portefolio` (`id_portefolio`, `picture_portefolio`, `originPicture_portefolio`, `evenement_portefolio`, `lieu_portefolio`, `date_portefolio`, `other_portefolio`) VALUES
+(6, '', ' ', ' Un portefolio', 'Lyon ', '1800-07-14', 'Oui ca marche');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
